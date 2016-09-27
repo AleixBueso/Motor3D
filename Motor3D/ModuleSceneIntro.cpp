c784740a4f817b1756b66693bf00fec4a279f2c0
Aleix Bueso <aleix.bueso@gmail.com>
2016-09-27 21:57:19 +0200

    Homework 27/9

--- Motor3D/ModuleSceneIntro.cpp
+++ Motor3D/ModuleSceneIntro.cpp
@@ -27,61 +27,62 @@ bool ModuleSceneIntro::Start()
 {
 	LOG("Loading Intro assets");
 	bool ret = true;
+
+	
+	vector<FixedPoint<float, sizeof(float)>>* vertices = vector<float4> cube_vertices;
+
+	cube_vertices.push_back(FixedPoint<float, sizeof(float)>(0.f, 0.f, 0.f));
+	cube_vertices.push_back(FixedPoint<float, sizeof(float)>(0.f, 2.f, 0.f));
+	cube_vertices.push_back(FixedPoint<float, sizeof(float)>(0.f, 0.f, 2.f));
+
+	glVertex3f(0.f, 2.f, 0.f);
+	glVertex3f(0.f, 2.f, 2.f);
+	glVertex3f(0.f, 0.f, 2.f);
+
+	glVertex3f(0.f, 2.f, 0.f);
+	glVertex3f(-2.f, 2.f, 2.f);
+	glVertex3f(0.f, 2.f, 2.f);
+
+	glVertex3f(0.f, 2.f, 0.f);
+	glVertex3f(-2.f, 2.f, 0.f);
+	glVertex3f(-2.f, 2.f, 2.f);
+
+	glVertex3f(0.f, 0.f, 0.f);
+	glVertex3f(-2.f, 2.f, 0.f);
+	glVertex3f(0.f, 2.f, 0.f);
+
+	glVertex3f(0.f, 0.f, 0.f);
+	glVertex3f(-2.f, 0.f, 0.f);
+	glVertex3f(-2.f, 2.f, 0.f);
+
+	glVertex3f(-2.f, 0.f, 0.f);
+	glVertex3f(-2.f, 2.f, 2.f);
+	glVertex3f(-2.f, 2.f, 0.f);
+
+	glVertex3f(-2.f, 0.f, 0.f);
+	glVertex3f(-2.f, 0.f, 2.f);
+	glVertex3f(-2.f, 2.f, 2.f);
+
+	glVertex3f(-2.f, 0.f, 2.f);
+	glVertex3f(0.f, 0.f, 2.f);
+	glVertex3f(0.f, 2.f, 2.f);
+
+	glVertex3f(-2.f, 0.f, 2.f);
+	glVertex3f(0.f, 2.f, 2.f);
+	glVertex3f(-2.f, 2.f, 2.f);
+
+	glVertex3f(0.f, 0.f, 0.f);
+	glVertex3f(0.f, 0.f, 2.f);
+	glVertex3f(-2.f, 0.f, 0.f);
+
+	glVertex3f(0.f, 0.f, 2.f);
+	glVertex3f(-2.f, 0.f, 2.f);
+	glVertex3f(-2.f, 0.f, 0.f);
+
 	uint my_id = 0;
-
-	//vector<Vector3> vertices;
-
-	glVertex3f(0.f, 0.f, 0.f);
-	glVertex3f(0.f, 2.f, 0.f);
-	glVertex3f(0.f, 0.f, 2.f);
-
-	glVertex3f(0.f, 2.f, 0.f);
-	glVertex3f(0.f, 2.f, 2.f);
-	glVertex3f(0.f, 0.f, 2.f);
-
-	glVertex3f(0.f, 2.f, 0.f);
-	glVertex3f(-2.f, 2.f, 2.f);
-	glVertex3f(0.f, 2.f, 2.f);
-
-	glVertex3f(0.f, 2.f, 0.f);
-	glVertex3f(-2.f, 2.f, 0.f);
-	glVertex3f(-2.f, 2.f, 2.f);
-
-	glVertex3f(0.f, 0.f, 0.f);
-	glVertex3f(-2.f, 2.f, 0.f);
-	glVertex3f(0.f, 2.f, 0.f);
-
-	glVertex3f(0.f, 0.f, 0.f);
-	glVertex3f(-2.f, 0.f, 0.f);
-	glVertex3f(-2.f, 2.f, 0.f);
-
-	glVertex3f(-2.f, 0.f, 0.f);
-	glVertex3f(-2.f, 2.f, 2.f);
-	glVertex3f(-2.f, 2.f, 0.f);
-
-	glVertex3f(-2.f, 0.f, 0.f);
-	glVertex3f(-2.f, 0.f, 2.f);
-	glVertex3f(-2.f, 2.f, 2.f);
-
-	glVertex3f(-2.f, 0.f, 2.f);
-	glVertex3f(0.f, 0.f, 2.f);
-	glVertex3f(0.f, 2.f, 2.f);
-
-	glVertex3f(-2.f, 0.f, 2.f);
-	glVertex3f(0.f, 2.f, 2.f);
-	glVertex3f(-2.f, 2.f, 2.f);
-
-	glVertex3f(0.f, 0.f, 0.f);
-	glVertex3f(0.f, 0.f, 2.f);
-	glVertex3f(-2.f, 0.f, 0.f);
-
-	glVertex3f(0.f, 0.f, 2.f);
-	glVertex3f(-2.f, 0.f, 2.f);
-	glVertex3f(-2.f, 0.f, 0.f);
-
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glBindBuffer(GL_VERTEX_ARRAY, my_id);
-	glVertexPointer(3, GL_FLOAT, 0, NULL);
+	glGenBuffers(1, (GLuint*)&(my_id));
+	glBindBuffer(GL_ARRAY_BUFFER, my_id);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float)* 12 * 3, vertices, GL_STATIC_DRAW);
 
 	return ret;
 }
@@ -100,7 +101,7 @@ update_status ModuleSceneIntro::Update(float dt)
 	CreateCubePrimitive();
 
 	glDrawArrays(GL_TRIANGLES, 0, 12 * 3 * 3);
-	//glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
 
 	return UPDATE_CONTINUE;
 }
